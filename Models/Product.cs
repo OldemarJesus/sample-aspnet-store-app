ca03bd2ba608d542f04f87fb7cbf28730fcf990e
Oldemar <oldemego@gmail.com>
2024-01-29 20:59:21 +0000

    intial commit

---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace sample_website_aspnet;
+
+public class Product
+{
+    [JsonPropertyName("Id")]
+    public string? Id { get; set; }
+    [JsonPropertyName("Maker")]
+    public string? Maker { get; set; }
+    [JsonPropertyName("img")]
+    public string? Image { get; set; }
+    [JsonPropertyName("Url")]
+    public string? Url { get; set; }
+    [JsonPropertyName("Title")]
+    public string? Title { get; set; }
+    [JsonPropertyName("Description")]
+    public string? Description { get; set; }
+    public string? Rantings { get; set; }
+
+
+    public override string ToString() => JsonSerializer.Serialize(this);
+
+}
---
+++ Services/JsonFileProductService.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace sample_website_aspnet;
+
+public class JsonFileProductService
+{
+    public IWebHostEnvironment WebHostEnvironment { get; }
+    public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+    {
+        WebHostEnvironment = webHostEnvironment;
+    }
+
+
+    private string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+
+    public IEnumerable<Product>? GetProducts()
+    {
+        using var jsonFileReader = File.OpenText(JsonFileName);
+        return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+        new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+    }
+}
